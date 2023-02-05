562fef12c57368fbd3dfe28bb931c860c931b3f0
Eleaaa <60519357+Eleaaa@users.noreply.github.com>
2023-02-05 14:45:44 +0100

    level without items

--- js/levels.js
+++ js/levels.js
@@ -6,7 +6,7 @@ import { buildPowerUp } from "./powerups.js";
 const NB_WALL_TEXTURES = 7;
 
 
-const MAP0 = [
+/*const MAP0 = [
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,1,2,0,0,0,0,0,1],
     [1,0,4,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
@@ -31,7 +31,40 @@ const MAP0 = [
     [1,0,1,0,1,0,0,0,0,1,1,0,1,1,0,0,1,0,0,1,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,1,1,0,1,1,5,0,0,0,0,1,0,0,0,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-];
+];*/
+
+const MAP0 = [
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,1,1,5,0,0,0,1,0,0,0,0,0,1,0,4,1,5,0,1],
+    [1,0,0,0,0,1,1,0,0,0,0,3,1,1,0,0,0,0,0,4,1,5,0,0,0,1,1,1,0,1],
+    [1,0,0,0,0,1,1,1,1,5,0,0,0,0,0,0,1,0,0,1,1,1,0,0,0,3,1,2,0,1],
+    [1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,3,1,2,0,1,0,0,1,0,0,1],
+    [1,1,1,1,1,1,1,1,1,2,0,0,1,1,1,1,1,5,0,0,0,0,0,0,0,0,1,0,0,1],
+    [1,1,1,1,1,2,0,0,0,0,0,0,1,0,0,0,3,1,5,0,1,0,4,1,1,1,1,5,0,1],
+    [1,1,1,2,0,0,0,0,0,0,0,0,1,0,0,0,0,3,1,5,0,4,1,2,0,0,0,3,1,1],
+    [1,1,2,0,0,4,1,1,5,0,0,0,1,1,1,1,5,0,3,1,1,1,2,0,4,1,5,0,0,1],
+    [1,2,0,0,4,1,1,1,1,5,0,0,3,1,1,1,1,5,0,3,1,2,0,4,1,1,2,0,0,1],
+    [1,0,0,4,1,1,1,1,1,1,5,0,0,3,1,1,1,2,0,0,1,0,4,1,1,2,0,1,0,1],
+    [1,0,0,1,1,1,1,1,2,3,1,0,0,0,0,0,0,0,0,0,1,0,1,1,2,0,0,0,0,1],
+    [1,0,1,1,1,0,0,0,0,0,3,5,0,0,0,0,0,0,0,0,1,0,1,0,0,0,1,5,0,1],
+    [1,0,1,0,1,0,1,5,0,0,0,1,0,0,1,1,1,1,0,1,1,0,1,0,1,0,3,1,0,1],
+    [1,0,0,0,1,0,1,1,1,0,0,1,0,0,1,0,0,1,0,0,1,0,1,0,1,5,0,0,0,1],
+    [1,0,1,0,1,0,4,1,1,0,0,1,0,0,1,0,1,1,0,0,1,0,1,0,3,1,5,0,0,1],
+    [1,0,1,1,1,1,1,1,1,0,0,1,0,0,1,0,1,1,0,4,1,1,1,5,0,1,1,1,1,1],
+    [1,0,3,1,0,0,0,1,2,0,0,1,0,0,1,0,1,1,0,1,0,0,0,1,0,0,0,0,0,1],
+    [1,0,0,3,1,0,1,2,0,1,0,1,0,0,1,0,1,1,0,1,0,1,1,1,5,0,1,1,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,1,0,0,1,0,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,0,1,0,1,0,1,2,0,4,1,0,1],
+    [1,1,1,1,1,1,1,1,1,1,1,2,0,0,3,1,0,1,1,2,0,1,0,1,0,4,1,1,0,1],
+    [1,1,1,1,1,1,1,1,1,1,2,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,1],
+    [1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,2,0,0,0,1],
+    [1,0,0,4,1,1,1,1,1,1,1,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,4,1,5,1],
+    [1,0,0,1,1,0,0,0,0,1,1,0,1,5,0,0,0,0,0,0,0,4,1,5,0,4,1,0,1,1],
+    [1,0,0,3,1,1,1,0,1,1,0,0,0,3,1,1,1,1,1,1,1,1,0,1,1,1,0,0,0,1],
+    [1,5,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+  ]
 
 const MAP1 = [
     [1,1,1,1,1,1,1,1,1],
@@ -70,6 +103,64 @@ const ENEMIES2 = function() {
     return [dino0, dino1];*/
 };
 
+const ENEMIES4 = function() {
+
+    // tree running around
+    const tree0 = buildEnemy("dandelion", 3.5, 5.5, 0, 1);
+    /*stree0.behavior = function() {
+        if (!this.xTarget || !this.yTarget) {
+            this.xTarget = 3.5;
+            this.yTarget = 7.5;
+            this.walk();
+        }
+        if ((this.x-this.xTarget)*(this.x-this.xTarget) + (this.y-this.yTarget)*(this.y-this.yTarget) < 0.1) {
+            this.dirY = -this.dirY;
+            this.yTarget += this.dirY * 4;
+            this.angle = (this.angle + 180) % 360;
+        }
+    }.bind(tree0);*/
+
+    // static tree
+    const tree1 = buildEnemy("dandelion", 3.5, 10.5, 0, 1);
+
+    // tree running around
+    const turnip0 = buildEnemy("tree", 6.5, 16.5, 0, 1);
+    
+
+    // static tree
+    const turnip1 = buildEnemy("tree", 4.5, 14.5, 0, 1);
+    turnip1.behavior = function() {
+        if (!this.xTarget || !this.yTarget) {
+            this.xTarget = 4.5;
+            this.yTarget = 14.5;
+            this.walk();
+        }
+        if ((this.x-this.xTarget)*(this.x-this.xTarget) + (this.y-this.yTarget)*(this.y-this.yTarget) < 0.1) {
+            this.dirY = -this.dirY;
+            this.yTarget += this.dirY * 4;
+            this.angle = (this.angle + 180) % 360;
+        }
+    }.bind(turnip1);
+    
+    // Whisky power up
+    const whisky0 = buildPowerUp("whisky", 3.5, 20.5);
+    const whisky1 = buildPowerUp("whisky", 3.5, 19.5);
+    const whisky2 = buildPowerUp("whisky", 3.5, 18.5);
+    const whisky3 = buildPowerUp("whisky", 3.5, 17.5);
+    const whisky4 = buildEnemy("tree", 3.5, 13.5,0,0);
+
+    // Tequila power up
+    const tequila0 = buildPowerUp("tequila", 2.5, 20.5);
+
+    // Uique carrot
+    const carrot = buildPowerUp("carrot", 6.5, 20.5);
+
+    // Uique rabbit and dialog
+    const rabbit = buildPowerUp("rabbit", 10.5, 20.5);
+    const dialog = buildPowerUp("dialog", 10.5, 20.5); // same position as rabit
+
+    return [tree0, tree1, /*turnip1,*/ whisky0, whisky1, whisky2, whisky3, whisky4, tequila0, carrot, rabbit, dialog];
+};
 
 const ENEMIES3 = function() {
 
@@ -142,10 +233,10 @@ export const levels = {
         enemies: ENEMIES2
     },
     "tree": {
-        player: { posX: 3.5, posY: 12.5, dirX: 0, dirY: 1 },
+        player: { posX: 2.5, posY: 26.5, dirX: 1, dirY: 0 },
         map: invert(MAP0),
         textures: mkTextures(invert(MAP0)),
-        enemies: ENEMIES3
+        enemies: ENEMIES4
     },
     "test": {
         player: { posX: 2.5, posY: 2.5, dirX: 1, dirY: 0 },
